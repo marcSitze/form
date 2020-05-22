db36da347764c2ebae2ef85187e115a3798341a8
marc sitze <marcsitze01@gmail.com>
2020-05-22 15:54:10 +0100

    Setup update db

--- server.js
+++ server.js
@@ -11,12 +11,15 @@ const zachRoute = require('./routes/zach');
 // Set view engine
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-
+const database_url = 'mongodb+srv://marc:xTBC19OEWaGOd7Dz@localdb-2oyid.mongodb.net/test?retryWrites=true&w=majoritymongodb+srv://marc:N935vQe8.$hGT3F@localdb-2oyid.mongodb.net/test?retryWrites=true&w=majority';
+//const database = 'mongodb://marc:xTBC19OEWaGOd7Dz@main-shard-00-00-03xkr.mongodb.net:27017,main-shard-00-01-03xkr.mongodb.net:27017,main-shard-00-02-03xkr.mongodb.net:27017/main?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true';
 // Connect to the db
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(database_url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+})
+.then(() => console.log('mongodb server connected'))
+.catch(err => console.error(err));
 const db = mongoose.connection;
 db.on('error', err => console.error(err));
 db.once('open', () => console.log('Mongodb Connected...'));
